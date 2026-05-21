0ad482dbb173a09e9e49ad4ceaaaf18d6cb32298
mmasaki0 <111476395+mmasaki0@users.noreply.github.com>
2026-05-21 01:37:59 -0500

    switch to gxepd2 lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,28 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-#include "Display_EPD_W21_spi.h"
-#include "Display_EPD_W21.h"
-// put function declarations here:
+#include <GxEPD2_BW.h>
+#include <Fonts/FreeMono12pt7b.h>
+
+#define EPD_BUSY 4
+#define EPD_RST 16
+#define EPD_DC 17
+#define EPD_CS 5
+
+GxEPD2_BW<GxEPD2_370_GDEY037T03, GxEPD2_370_GDEY037T03::HEIGHT> display(GxEPD2_370_GDEY037T03(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
 
 void setup() {
-   pinMode(4, INPUT);  //BUSY
-   pinMode(16, OUTPUT); //RES 
-   pinMode(17, OUTPUT); //DC   
-   pinMode(5, OUTPUT); //CS
-
-   //SPI
-   SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0)); 
-   SPI.begin(18, -1, 23, 5);
-
-   Serial.begin(115200);
+  display.init(115200);
+  display.setRotation(1);
+  display.setFont(&FreeMono12pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  display.setCursor(100,100);
+  display.print("yo");
+  display.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // digitalWrite(14, LOW);
-  // delay(2000);
-  // digitalWrite(14, HIGH);
-  // delay(2000);
+
+
+  esp_deep_sleep_start();
 }
