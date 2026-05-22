08e538c91ad42a8bd47f5f14cb5069dfac71d89f
mmasaki0 <111476395+mmasaki0@users.noreply.github.com>
2026-05-21 21:31:56 -0500

    bluetooth device scan

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
-#include <SPI.h>
 
 #include <GxEPD2_BW.h>
 #include <Fonts/FreeMono12pt7b.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+#include <BluetoothSerial.h>
+#include <BluetoothA2DPSource.h>
 
 #define EPD_BUSY 4
 #define EPD_RST 16
@@ -11,18 +14,45 @@
 
 GxEPD2_BW<GxEPD2_370_GDEY037T03, GxEPD2_370_GDEY037T03::HEIGHT> display(GxEPD2_370_GDEY037T03(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
 
+BluetoothSerial SerialBT;
+BluetoothA2DPSource a2dp;
+
+void btDeviceFound(BTAdvertisedDevice *device) {
+  Serial.printf(device->getName().c_str());
+}
+
 void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32", true);
+  //bluetooth scan
+  if(SerialBT.discoverAsync(btDeviceFound, 10000)) {
+    Serial.printf("started scanning");
+    SerialBT.discoverAsyncStop();
+    Serial.printf("stopped scanning");
+  }
+
+  //eink display
   display.init(115200);
-  display.setRotation(1);
-  display.setFont(&FreeMono12pt7b);
+  delay(1000);
+
+  int16_t x1, y1;
+  uint16_t w, h;
+
+  display.setRotation(2);
   display.setTextColor(GxEPD_BLACK);
-  display.setCursor(100,100);
-  display.print("yo");
-  display.display();
+  display.setFont(&FreeMonoBold9pt7b);
+
+  display.getTextBounds("yo", 100, 100, &x1, &y1, &w, &h);
+
+  display.setPartialWindow(x1, y1, w, h);
+
+  display.firstPage();
+    do {
+      display.setCursor(100, 100);
+      display.print("yo");
+    } while (display.nextPage()); // Automatically renders and flushes over SPI
+  display.hibernate();
 }
 
 void loop() {
-
-
-  esp_deep_sleep_start();
 }
