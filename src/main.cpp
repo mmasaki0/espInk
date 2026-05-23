ca9dfce2b8d359151ab97e3ce66db836345be9d8
mmasaki0 <111476395+mmasaki0@users.noreply.github.com>
2026-05-22 21:14:29 -0500

    audio playback with callback

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,15 @@
 #include <BluetoothSerial.h>
 #include <BluetoothA2DPSource.h>
 
-#include <Wifi.h>
-#include <WebServer.h>
-#include "FS.h"
-#include <LittleFS.h>
+// #include <Wifi.h>
+// #include <WebServer.h>
+// #include "FS.h"
+// #include <LittleFS.h>
 #include <config.h>
+
+#include <AudioTools.h>
+#include <AudioTools/Communication/A2DPStream.h>
+#include <AudioTools/AudioCodecs/CodecMP3Helix.h>
 
 #define EPD_BUSY 4
 #define EPD_RST 16
@@ -23,14 +27,25 @@
 
 #define FORMAT_LITTLEFS_IF_FAILED true
 
-WebServer server(80);
+// WebServer server(80);
 
 GxEPD2_BW<GxEPD2_370_GDEY037T03, GxEPD2_370_GDEY037T03::HEIGHT> display(GxEPD2_370_GDEY037T03(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
 
 
 
 BluetoothSerial SerialBT;
-BluetoothA2DPSource a2dp;
+BluetoothA2DPSource a2dp_source;
+
+File song1;
+MP3DecoderHelix mp3;
+EncodedAudioStream decoder(&song1, &mp3);
+ResampleStream resampler(decoder);
+
+int32_t get_sound_data(uint8_t* data, int32_t size) {
+  int32_t result = decoder.readBytes((uint8_t*)data, size);
+  delay(1);
+  return(result);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -40,101 +55,119 @@ void setup() {
     return;
   }
 
+  Serial.println("Starting audio");
+  song1 = SD.open("/library/ARIRANG/SWIM2.mp3");
 
+  decoder.transformationReader().resizeResultQueue(1024 * 8);
+
+  if (!decoder.begin()) {
+    Serial.println("decoder failed");
+    stop();
+  }
   
+  auto rcfg = resampler.defaultConfig();
+  rcfg.copyFrom(decoder.audioInfo());
+  rcfg.sample_rate = 44100;
+  resampler.begin(rcfg);
 
-  //begins file system, stops setup() if fails
-  if(!LittleFS.begin(true)) {
-    Serial.println("Error during LittleFS mount.");
-    return;
-  }
+  a2dp_source.set_data_callback(get_sound_data);
+  a2dp_source.start("hachiware - Find My");
 
-  SerialBT.begin("ESP32", true);
+  Serial.println((double)decoder.audioInfo().sample_rate);
 
-  //wifi and webserver 
-  WiFi.begin(SSID, Password);
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+  // //begins file system, stops setup() if fails
+  // if(!LittleFS.begin(true)) {
+  //   Serial.println("Error during LittleFS mount.");
+  //   return;
+  // }
 
-  const String localIP = WiFi.localIP().toString();
+  // SerialBT.begin("ESP32", true);
 
-  server.on("/", []() {
-    Serial.println("connected to webserver");
-    if(LittleFS.exists("/index.html")) {
-      Serial.println("found index");
-      File fileIndex = LittleFS.open("/index.html", "r");
-      server.streamFile(fileIndex, "text/html");
-      fileIndex.close();
-    } else {
-      Serial.println("did not find index");
-    }
-  });
+  // //wifi and webserver 
+  // WiFi.begin(SSID, Password);
+  // while(WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  // }
 
-  server.onNotFound([]() {
-    Serial.println("webserver not found");
-    server.send(404, "text/plain", "Not found");
-  });
+  // const String localIP = WiFi.localIP().toString();
 
-  server.begin();
+  // server.on("/", []() {
+  //   Serial.println("connected to webserver");
+  //   if(LittleFS.exists("/index.html")) {
+  //     Serial.println("found index");
+  //     File fileIndex = LittleFS.open("/index.html", "r");
+  //     server.streamFile(fileIndex, "text/html");
+  //     fileIndex.close();
+  //   } else {
+  //     Serial.println("did not find index");
+  //   }
+  // });
+
+  // server.onNotFound([]() {
+  //   Serial.println("webserver not found");
+  //   server.send(404, "text/plain", "Not found");
+  // });
+
+  // server.begin();
 
   //bluetooth scan
-  if(SerialBT.discoverAsync([](BTAdvertisedDevice *device) {Serial.printf(device->getName().c_str());})) {
-    Serial.printf("started scanning");
-    delay(10000);
-    SerialBT.discoverAsyncStop();
-    Serial.printf("stopped scanning");
-  }
-
-  
+  // if(SerialBT.discoverAsync([](BTAdvertisedDevice *device) {Serial.printf(device->getName().c_str());})) {
+  //   Serial.printf("started scanning");
+  //   delay(10000);
+  //   SerialBT.discoverAsyncStop();
+  //   Serial.printf("stopped scanning");
+  // }
 
   //eink display
-  display.init(115200);
-  delay(1000);
+  // display.init(115200);
+  // delay(1000);
+  // display.hibernate();
+ 
 
-  int16_t x1, y1;
-  uint16_t w, h;
-
-  display.setRotation(2);
-  display.setTextColor(GxEPD_BLACK);
-  display.setFont(&FreeMonoBold9pt7b);
-
-  display.getTextBounds(localIP, 100, 100, &x1, &y1, &w, &h);
-
-  display.setPartialWindow(x1, y1, w, h);
-
-  display.firstPage();
-    do {
-      display.setCursor(100, 100);
-      display.println(localIP);
-    } while (display.nextPage());
-
-  String songsList = "";
-
-  File dirLibrary = SD.open("/library/ARIRANG");
-  while(true) {
-    File entry = dirLibrary.openNextFile();
-    if(!entry) {
-      break;
-    }
-    songsList += entry.name();
-  }
-
-
-  display.getTextBounds(songsList, 100, 120, &x1, &y1, &w, &h);
-  display.setPartialWindow(x1, y1, w, h);
-  display.firstPage();
-    do {
-      display.setCursor(100, 120);
-      display.println(songsList);
-    } while (display.nextPage());
-
-  display.hibernate();
 }
+
+  // int16_t x1, y1;
+  // uint16_t w, h;
+
+  // display.setRotation(2);
+  // display.setTextColor(GxEPD_BLACK);
+  // display.setFont(&FreeMonoBold9pt7b);
+
+  // display.getTextBounds(localIP, 100, 100, &x1, &y1, &w, &h);
+
+  // display.setPartialWindow(x1, y1, w, h);
+
+  // display.firstPage();
+  //   do {
+  //     display.setCursor(100, 100);
+  //     display.println(localIP);
+  //   } while (display.nextPage());
+
+  // String songsList = "";
+
+  // File dirLibrary = SD.open("/library/ARIRANG");
+  // while(true) {
+  //   File entry = dirLibrary.openNextFile();
+  //   if(!entry) {
+  //     break;
+  //   }
+  //   songsList += entry.name();
+  // }
+
+
+  // display.getTextBounds(songsList, 100, 120, &x1, &y1, &w, &h);
+  // display.setPartialWindow(x1, y1, w, h);
+  // display.firstPage();
+  //   do {
+  //     display.setCursor(100, 120);
+  //     display.println(songsList);
+  //   } while (display.nextPage());
+
+
 
 
 
 void loop() {
-  server.handleClient();
+  // server.handleClient();
 }
